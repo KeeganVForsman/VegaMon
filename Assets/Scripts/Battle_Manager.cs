b13246eb39c470b8c1d4a21303912486721134c5
DuncanSmit01 <88308215+DuncanSmit01@users.noreply.github.com>
2023-04-11 12:44:56 +0200

    Better Ui working
    Displays names and working buttons

--- Assets/Scripts/Battle_Manager.cs
+++ Assets/Scripts/Battle_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BattleStates { START, PLAYER_1, PLAYER_2, WIN, LOSE, }
 
@@ -10,8 +11,15 @@ public class Battle_Manager : MonoBehaviour
     public GameObject Player_1;
     public GameObject Player_2;
 
+    Unit_Script Player_1Unit;
+    Unit_Script Player_2Unit;
+
     public Transform Player_1Spawn;
     public Transform Player_2Spawn;
+
+    public Text Creature_1Name;
+    public Text Creature_2Name;
+    public Text Dialogue;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +36,29 @@ public class Battle_Manager : MonoBehaviour
 
     void startBattle()
     {
-        Instantiate(Player_1, Player_1Spawn);
-        Instantiate(Player_2, Player_2Spawn);
+        GameObject Player_1Turn = Instantiate(Player_1, Player_1Spawn);
+        Player_1Unit = Player_1Turn.GetComponent<Unit_Script>();
+
+        GameObject Player_2Turn = Instantiate(Player_2, Player_2Spawn);
+        Player_2Unit = Player_2Turn.GetComponent<Unit_Script>();
+
+        Creature_1Name.text = Player_1Unit.creatureName;
+
+        Creature_2Name.text = Player_2Unit.creatureName;
+
+        Dialogue.text = "Let the battle begin.";
+
+        states = BattleStates.PLAYER_1;
+        Player_1BattleTurn();
+    }
+
+    public void RandomNumber()
+    {
+
+    }
+
+    public void Player_1BattleTurn()
+    {
+        Dialogue.text = "Its Player 1's turn.... Please choose an action.";
     }
 }
